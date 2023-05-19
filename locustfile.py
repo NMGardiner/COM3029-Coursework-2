d02876dc64262c1c4cfcc542f5e4affe0bf9f72f
Ryan Slater <ryan.slater157@gmail.com>
2023-05-19 13:37:13 +0100

    added volume test and calling from python script

--- locustfile.py
+++ locustfile.py
@@ -4,9 +4,15 @@ from random import randint
 
 
 class StressTest(HttpUser):
+    host = 'http://localhost:5000'
     dataset = EmotionsDataset(split='test')
+    longest_comment = max(dataset.x, key=len)
 
-    @task
-    def get_prediction(self):
+    @task(1)
+    def load_test(self):
         random_comment = randint(0, len(self.dataset)-1)
         self.client.post(url='/', json={'comment':self.dataset[random_comment][0]})
+
+    @task(2)
+    def volume_test(self):
+        self.client.post(url='/', json={'comment':self.longest_comment})
